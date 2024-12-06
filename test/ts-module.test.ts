e70abb3546580d3a6ffdeb2ff42fc03492626d3d
John Wright <jrite@protonmail.com>
2024-12-06 10:39:37 +0000

    feat: init

--- test/ts-module.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from 'vitest'
-import { tsModule } from '@johngeorgewright/ts-module/foo'
-
-test("it's a module", () => {
-  expect(tsModule()).toBe('I am a TypeScript module')
-})
